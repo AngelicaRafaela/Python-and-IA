5012e00dc8104e8520f9b7f6b4c5d43e764d8751
Angélica Rafaela <angelmaluh@gmail.com>
2024-04-22 17:16:42 -0300

    Modify polimorfismo

--- 02 - Programação Orientada a Objetos/06 - Polimorfismo/01_polimorfismo.py
+++ 02 - Programação Orientada a Objetos/06 - Polimorfismo/01_polimorfismo.py
@@ -1,28 +1,34 @@
 class Passaro:
     def voar(self):
+        # Método que define o comportamento de voar genérico para pássaros
         print("Voando...")
 
 
 class Pardal(Passaro):
     def voar(self):
+        # Método que substitui o comportamento de voar para o pardal
         print("Pardal pode voar")
 
 
 class Avestruz(Passaro):
     def voar(self):
+        # Método que substitui o comportamento de voar para a avestruz
         print("Avestruz não pode voar")
 
 
-# NOTE: exemplo ruim do uso de herança para "ganhar" o método voar
+# Exemplo ruim do uso de herança para "ganhar" o método voar
 class Aviao(Passaro):
     def voar(self):
+        # Método que substitui o comportamento de voar para o avião
         print("Avião está decolando...")
 
 
 def plano_voo(obj):
+    # Função que recebe um objeto e chama o método voar do objeto
     obj.voar()
 
 
-plano_voo(Pardal())
-plano_voo(Avestruz())
-plano_voo(Aviao())
+# Cria instâncias de cada classe e chama a função plano_voo para cada uma delas
+plano_voo(Pardal())  # O pardal pode voar
+plano_voo(Avestruz())  # A avestruz não pode voar
+plano_voo(Aviao())  # O avião está decolando
